b2f4b93a7d7adc600bfab44c4015352edb440962
adilhannouni <adilhannouni@gmail.com>
2020-09-22 01:15:00 -0700

    Add Routing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+//Components
+import Home from './components/home';
+import Profiles from './components/profiles';
+import Posts from './components/posts';
+
+const App = () => {
+return (
+  <BrowserRouter>
+      <div>
+        <header>
+          <Link to='/'>Home</Link><br/>
+          <Link to='/posts'>Posts</Link><br/>
+          <Link to={{
+            pathname: '/profiles',
+            hash: '#entryArgument',
+            search: '?index=1'
+          }}>Profiles</Link>
+        </header>
+        <Route path="/" exact component={Home}/> 
+        <Route path="/posts" component={Posts}/> 
+        <Route path="/profiles" component={Profiles}/> 
+      </div>
+  </BrowserRouter>
+)
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  <App/>, document.querySelector('#root')
+)

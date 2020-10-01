2bd4b9bce8e3b4fc27148f97a5d67303c24e953f
adilhannouni <adilhannouni@gmail.com>
2020-09-30 23:41:42 -0700

    Add Redirect to a specific page

--- src/components/profiles.js
+++ src/components/profiles.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Profiles = (props) => {
+  const redirectUrl = () => {
+    props.history.push('/')
+  }
   return (
     <div>
       <Link to={{
         pathname: `${props.match.url}/posts`
       }}>
     
-    routing to Profiles/Posts</Link>
+    routing to Profiles/Posts2</Link>
+    {redirectUrl()}
     </div>
   )
 }
